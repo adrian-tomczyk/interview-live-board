0b99192a79cbbd0699ee8f49206a28d065c43dff
Adrian Tomczyk <adrian.tomczyk@yahoo.com>
2025-09-20 22:44:43 +0200

    LiveMatches - order of matches sorted by score and creation test

--- src/test/java/com/tomczyk/board/livematches/LiveMatchesTest.java
+++ src/test/java/com/tomczyk/board/livematches/LiveMatchesTest.java
@@ -5,9 +5,22 @@ import com.tomczyk.board.match.MatchEvent;
 import com.tomczyk.board.match.MatchEventType;
 import org.junit.jupiter.api.Test;
 
+import java.util.stream.IntStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class LiveMatchesTest {
+
+    private static void repeatScoreEvent(LiveMatches liveMatches, int times, MatchEventType matchEventType, String home, String away) {
+        IntStream.range(0, times).forEach(i -> {
+            MatchEvent matchEvent = new MatchEvent(matchEventType, home, away);
+            try {
+                liveMatches.updateMatchScore(matchEvent);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
 
     @Test
     public void shouldInitWithEmptyMatchesList() {
@@ -329,4 +342,101 @@ public class LiveMatchesTest {
         assertEquals("Poland", match4.getHomeName());
         assertEquals("Uruguay", match4.getAwayName());
     }
+
+    @Test
+    public void shouldOrderMatchesByStartEvent() throws Exception {
+        //given
+        LiveMatches liveMatches = new LiveMatches();
+
+        MatchEvent matchStartEvent1 = new MatchEvent(MatchEventType.MATCH_STARTED, "Poland", "Uruguay");
+        MatchEvent matchStartEvent2 = new MatchEvent(MatchEventType.MATCH_STARTED, "Mexico", "Canada");
+        MatchEvent matchStartEvent3 = new MatchEvent(MatchEventType.MATCH_STARTED, "Germany", "France");
+        MatchEvent matchStartEvent4 = new MatchEvent(MatchEventType.MATCH_STARTED, "Argentina", "Australia");
+
+        liveMatches.addMatch(matchStartEvent1);
+        liveMatches.addMatch(matchStartEvent2);
+        liveMatches.addMatch(matchStartEvent3);
+        liveMatches.addMatch(matchStartEvent4);
+
+        //when
+        Match match1 = liveMatches.getCurrentMatches().getFirst();
+        Match match2 = liveMatches.getCurrentMatches().get(1);
+        Match match3 = liveMatches.getCurrentMatches().get(2);
+        Match match4 = liveMatches.getCurrentMatches().get(3);
+
+
+        //then
+        assertEquals("Poland", match1.getHomeName());
+        assertEquals("Uruguay", match1.getAwayName());
+
+        assertEquals("Mexico", match2.getHomeName());
+        assertEquals("Canada", match2.getAwayName());
+
+        assertEquals("Germany", match3.getHomeName());
+        assertEquals("France", match3.getAwayName());
+
+        assertEquals("Argentina", match4.getHomeName());
+        assertEquals("Australia", match4.getAwayName());
+    }
+
+
+    @Test
+    public void shouldOrderMatchesByScoreAndThenStartEvent() throws Exception {
+        //given
+        LiveMatches liveMatches = new LiveMatches();
+
+        MatchEvent matchStartEvent1 = new MatchEvent(MatchEventType.MATCH_STARTED, "Mexico", "Canada");
+        MatchEvent matchStartEvent2 = new MatchEvent(MatchEventType.MATCH_STARTED, "Spain", "Brazil");
+        MatchEvent matchStartEvent3 = new MatchEvent(MatchEventType.MATCH_STARTED, "Germany", "France");
+        MatchEvent matchStartEvent4 = new MatchEvent(MatchEventType.MATCH_STARTED, "Uruguay", "Italy");
+        MatchEvent matchStartEvent5 = new MatchEvent(MatchEventType.MATCH_STARTED, "Argentina", "Australia");
+
+        liveMatches.addMatch(matchStartEvent1);
+        liveMatches.addMatch(matchStartEvent2);
+        liveMatches.addMatch(matchStartEvent3);
+        liveMatches.addMatch(matchStartEvent4);
+        liveMatches.addMatch(matchStartEvent5);
+
+        repeatScoreEvent(liveMatches, 5, MatchEventType.AWAY_TEAM_SCORES, "Mexico","Canada");
+
+        repeatScoreEvent(liveMatches, 10, MatchEventType.HOME_TEAM_SCORES, "Spain","Brazil");
+        repeatScoreEvent(liveMatches, 2, MatchEventType.AWAY_TEAM_SCORES, "Spain","Brazil");
+
+        repeatScoreEvent(liveMatches, 2, MatchEventType.HOME_TEAM_SCORES, "Germany","France");
+        repeatScoreEvent(liveMatches, 2, MatchEventType.AWAY_TEAM_SCORES, "Germany","France");
+
+
+        repeatScoreEvent(liveMatches, 6, MatchEventType.HOME_TEAM_SCORES, "Uruguay","Italy");
+        repeatScoreEvent(liveMatches, 6, MatchEventType.AWAY_TEAM_SCORES, "Uruguay","Italy");
+
+        repeatScoreEvent(liveMatches, 3, MatchEventType.HOME_TEAM_SCORES, "Argentina","Australia");
+        repeatScoreEvent(liveMatches, 1, MatchEventType.AWAY_TEAM_SCORES, "Argentina","Australia");
+
+
+        //when
+        Match match1 = liveMatches.getCurrentMatches().getFirst();
+        Match match2 = liveMatches.getCurrentMatches().get(1);
+        Match match3 = liveMatches.getCurrentMatches().get(2);
+        Match match4 = liveMatches.getCurrentMatches().get(3);
+        Match match5 = liveMatches.getCurrentMatches().get(4);
+
+
+        //then
+        assertEquals("Uruguay", match1.getHomeName());
+        assertEquals("Italy", match1.getAwayName());
+
+        assertEquals("Spain", match2.getHomeName());
+        assertEquals("Brazil", match2.getAwayName());
+
+        assertEquals("Mexico", match3.getHomeName());
+        assertEquals("Canada", match3.getAwayName());
+
+        assertEquals("Argentina", match4.getHomeName());
+        assertEquals("Australia", match4.getAwayName());
+
+        assertEquals("Germany", match5.getHomeName());
+        assertEquals("France", match5.getAwayName());
+    }
+
+
 }
