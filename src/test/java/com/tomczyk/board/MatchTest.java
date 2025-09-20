b7ed2bbc238748a4818bbca626947da2289c77f1
Adrian Tomczyk <adrian.tomczyk@yahoo.com>
2025-09-20 18:01:27 +0200

    Initial commit, Match tests

---
+++ src/test/java/com/tomczyk/board/MatchTest.java
@@ -0,0 +1,45 @@
+package com.tomczyk.board;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MatchTest {
+
+    @Test
+    void shouldInitializeGameWithProperScore() {
+        //given
+        //when
+        Match match = new Match("Mexico", "Canada");
+
+        //then
+        assertEquals("Mexico - Canada: 0-0", match.getScore());
+
+    }
+
+    @Test
+    void shouldUpdateHomeScore() throws Exception {
+        //given
+        Match match = new Match("Mexico", "Canada");
+
+        //when
+        match.scoreHome();
+
+        //then
+        assertEquals("Mexico - Canada: 1-0", match.getScore());
+
+    }
+
+    @Test
+    void shouldUpdateAwayScore() throws Exception {
+        //given
+        Match match = new Match("Mexico", "Canada");
+
+        //when
+        match.scoreAway();
+
+        //then
+        assertEquals("Mexico - Canada: 0-1", match.getScore());
+    }
+}
